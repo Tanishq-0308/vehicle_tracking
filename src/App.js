f8a5a8107c160f3aa149636477acf8681c87364b
Tanishq <sharmatanishq0308@gmail.com>
2024-11-23 11:12:03 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
+import { GoogleMap, useJsApiLoader } from '@react-google-maps/api';
 import './App.css';
 
 function App() {
+  const { isLoaded } = useJsApiLoader({
+    googleMapsApiKey: 'AlzaSy_qSBDUZTrtbaBNuGMkXGOd26oQeeNK3FN', // Ensure your key is valid
+  });
+
+  if (!isLoaded) {
+    return <h2>Loading...</h2>;
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" style={{ height: '100vh' }}>
+      <GoogleMap mapContainerStyle={{ width: '100%', height: '100%' }}
+       center={{ lat: 37.7749, lng: -122.4194 }} // Example center point, update to your desired location
+       zoom={10} // Set a zoom level
+      > 
+
+      </GoogleMap>
+      {/* {isLoaded && <h2>not loadded</h2>} */}
     </div>
   );
 }
